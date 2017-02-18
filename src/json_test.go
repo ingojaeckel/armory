9b400290447206417e2106c57d9868db67f075ac
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2017-02-18 15:57:15 -0800

    more tests for json code

--- src/json_test.go
+++ src/json_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"io"
 	"io/ioutil"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -26,6 +27,28 @@ func TestUnitParseMalformedJson(t *testing.T) {
 
 	if err := parseJSON(getReadCloser(`{"artifact-url":`), &r); err == nil {
 		t.Error("This should have failed due to malformed JSON")
+	}
+}
+
+func TestUnitToJson(t *testing.T) {
+	r := FrontendResponse{"test-123"}
+	bytes, err := toJSON(r)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if string(bytes) != `{"id":"test-123"}` {
+		t.Errorf("Unexpected result of JSON conversion: %s", string(bytes))
+	}
+}
+
+func TestUnitWriteJSONAndLog(t *testing.T) {
+	resp := FrontendResponse{"123"}
+	w := httptest.NewRecorder()
+	if err := WriteJSONAndLog(w, 200, resp, true); err != nil {
+		t.Error(err.Error())
+	}
+	if w.Body.String() != `{"id":"123"}` {
+		t.Errorf("Unexpected response: %s", w.Body.String())
 	}
 }
 
