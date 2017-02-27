bd9f26245dfaa68a8878ce551fd0ccd1693d4bfe
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2017-02-27 11:04:33 -0800

    Use range in for loop to get rid of boilerplate code

--- src/worker.go
+++ src/worker.go
@@ -15,18 +15,13 @@ func startTestOnWorker(artifactURL string, simulationClass string, ipAddresses [
 	body := bytes.NewReader(jsonBytes)
 	client := &http.Client{}
 
-	for i := 0; i < len(ipAddresses); i++ {
-		ipAddress := ipAddresses[i]
-		req, err := http.NewRequest("PUT", fmt.Sprintf("http://%s:8080/rest/worker", *ipAddress), body)
-		if err != nil {
-			return err
-		}
-		// Start load test on each instance
+	for _, ipAddress := range ipAddresses {
+		req, _ := http.NewRequest("PUT", fmt.Sprintf("http://%s:8080/rest/worker", *ipAddress), body)
+		// Starts load test on the current worker instance
 		resp, err := client.Do(req)
 		if err != nil {
 			return err
 		}
-
 		if resp.StatusCode != http.StatusOK {
 			var errorResp ErrorResponse
 			if err := parseJSON(resp.Body, &errorResp); err != nil {
@@ -37,6 +32,5 @@ func startTestOnWorker(artifactURL string, simulationClass string, ipAddresses [
 			return fmt.Errorf("Request failed. Status code: %d, Description: %s", resp.StatusCode, errorResp.Description)
 		}
 	}
-
 	return nil
 }
