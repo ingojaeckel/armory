edcc87f6a63f44769afe61c752a88ce826fc897e
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2017-04-09 16:52:50 -0700

    simplified REST setup via goji framework

--- src/app.go
+++ src/app.go
@@ -5,39 +5,28 @@ import (
 	"io"
 	"log"
 	"net/http"
+
+	"goji.io"
+	"goji.io/pat"
 )
 
 func main() {
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Print(w, "up")
-	})
-	http.HandleFunc("/rest/test", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "PUT" {
-			handleTestPut(w, r)
-		} else if r.Method == "GET" {
-			handleTestGet(w, r)
-		} else {
-			w.WriteHeader(415)
-		}
-	})
-	http.HandleFunc("/rest/worker", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "PUT" {
-			handleWorkerPut(w, r)
-		} else {
-			w.WriteHeader(415)
-		}
-	})
-	http.HandleFunc("/rest/version", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			io.WriteString(w, Version)
-		} else {
-			w.WriteHeader(415)
-		}
-	})
 	if err := initConfiguration(); err != nil {
 		fmt.Printf("Failed to intialize the configuration: %s", err.Error())
 		return
 	}
+
+	mux := goji.NewMux()
+	mux.HandleFunc(pat.Get("/health"), func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, Version)
+	})
+	mux.HandleFunc(pat.Get("/rest/version"), func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, Version)
+	})
+	mux.HandleFunc(pat.Get("/rest/test"), handleTestGet)
+	mux.HandleFunc(pat.Put("/rest/test"), handleTestPut)
+	mux.HandleFunc(pat.Put("/rest/worker"), handleWorkerPut)
+
 	Log("Running..")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
